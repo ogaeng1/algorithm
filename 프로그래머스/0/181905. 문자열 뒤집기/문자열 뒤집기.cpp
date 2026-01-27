be758777ebb99d80e5e812d377141bed61a10dbd
Kyung Min OH <01092428351a@gmail.com>
2026-01-27 23:44:28 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -1,9 +1,10 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(string my_string, int s, int e) {
-    string answer = "";
-    return answer;
+    reverse(my_string.begin() + s, my_string.begin()+ e + 1);
+    return my_string;
 }
